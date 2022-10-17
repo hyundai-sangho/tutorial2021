48d81a843572c29bf5e0ec196ee06575d8c1c17c
조상호 <hyundai_sangho@naver.com>
2022-10-17 19:19:37 +0900

    JWT authentication 학습

---
+++ 11/controllers/logoutController.js
@@ -0,0 +1,34 @@
+const userDB = {
+  users: require('../model/users.json'),
+  setUsers: function (data) {
+    this.users = data;
+  },
+};
+const fsPromises = require('fs').promises;
+const path = require('path');
+
+const handleLogout = async (req, res) => {
+  // On client, also delete the accessToken
+  const cookies = req.cookies;
+  // 옵셔널 체이닝 문법
+  if (!cookies?.jwt) return res.sendStatus(204); // No content
+  const refreshToken = cookies.jwt;
+
+  // Is refreshToken in db?
+  const foundUser = userDB.users.find((person) => person.refreshToken === refreshToken);
+  if (!foundUser) {
+    res.clearCookie('jwt', { httpOnly: true });
+    return res.sendStatus(204);
+  }
+
+  // Delete refreshToken in db
+  const otherUsers = userDB.users.filter((person) => person.refreshToken !== foundUser.refreshToken);
+  const currentUser = { ...foundUser, refreshToken: '' };
+  userDB.setUsers([...otherUsers, currentUser]);
+  await fsPromises.writeFile(path.join(__dirname, '..', 'model', 'user.json'), JSON.stringify(userDB.users));
+
+  res.clearCookie('jwt', { httpOnly: true }); // secure: true - only serves on https
+  res.sendStatus(204);
+};
+
+module.exports = { handleLogout };
